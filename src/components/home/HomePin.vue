1e4fab198f80e9ac7e56458841a6698654855cd9
노정환 <zdsa43@gmail.com>
2022-11-22 17:32:25 +0900

    지도 중심기준 집 리스트 마커 출력

--- src/components/home/HomePin.vue
+++ src/components/home/HomePin.vue
@@ -6,6 +6,14 @@
       동코드 입력 : 
       <input type="text" id="dongCode" ref="dongCode" v-model.lazy="dongCode" />
       <button @click="getResults(dongCode)">동코드로 검색</button>
+      <br>
+      <p>{{address}}</p>
+      <!-- <input type="text" id="address" ref="address" v-model.lazy="address" /> -->
+    </div>
+    <div>
+      <input type="text" id="lat" ref="lat" v-model.lazy="lat" />
+      <input type="text" id="lng" ref="lng" v-model.lazy="lng" />
+      <button @click="updateLatLng()">중앙좌표 출력, 동코드 반환</button>
     </div>
     <div class="button-group">
       <button @click="displayMarker(markerPositions1)">marker set 1</button>
@@ -15,19 +23,21 @@
       <button @click="getResults(1111018300)">동코드 배열 콘솔 출력하기</button>
       <button @click="displayMarker(dongLatLng)">드뎌.. 맵에 찍기!</button>
       <button @click="displayInfoWindow">infowindow</button>
+      <button @click="currentLocationSearch()">현재위치 검색</button>
     </div>
-    <div id="map"></div>
+    <div id="map" @mouseup="updateLatLng()"  ></div> <!-- @mouseover="hello()" -->
+    <!-- {{console.log('이거슨 그냥 띄워보는거시다')}} -->
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import {mapActions, mapGetters} from "vuex";
 
 export default {
   name: "HomePin",
   data() {
     return {
-      dongCode: "",
       markerPositions1: [
         [33.452278, 126.567803],
         [33.452671, 126.574792],
@@ -46,6 +56,11 @@ export default {
       markerPositions4: [], //여기에 동코드로 검색한 주택정보 불러오자
       markers: [],
       infowindow: null,
+      lat:"",
+      lng:"",
+      dongCode: "",
+      address: "",
+      
     };
   },
   computed:{
@@ -69,10 +84,108 @@ export default {
   },
   methods: {
     ...mapActions(["getResults"]),
+
+
+    hello() { //마커에 마우스 올렸을 때 인포윈도우 출력
+      // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+      var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+      mapOption = { 
+          center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+          level: 3 // 지도의 확대 레벨
+      };
+
+      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+      // 마커를 표시할 위치입니다 
+      var position =  new kakao.maps.LatLng(33.450701, 126.570667);
+
+      // 마커를 생성합니다
+      var marker = new kakao.maps.Marker({
+        position: position
+      });
+
+      // 마커를 지도에 표시합니다.
+      marker.setMap(map);
+
+      // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+      var iwContent = '<div style="padding:5px;">Hello World!</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+
+      // 인포윈도우를 생성합니다
+      var infowindow = new kakao.maps.InfoWindow({
+          content : iwContent
+      });
+
+      // 마커에 마우스오버 이벤트를 등록합니다
+      kakao.maps.event.addListener(marker, 'mouseover', function() {
+        // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+          infowindow.open(map, marker);
+          console.log('마우스 올라갔다');
+      });
+
+      // 마커에 마우스아웃 이벤트를 등록합니다
+      kakao.maps.event.addListener(marker, 'mouseout', function() {
+          // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+          infowindow.close();
+          console.log('마우스 내렸다');
+      });
+      console.log('얼마나 출력됨?');
+    },
+
+
+
+
+    async currentLocationSearch() {
+      // console.log('!검색한 현재 주소 : ',this.address);
+      // console.log('!검색한 동코드 : ',this.dongCode);
+      // console.log('!현재 좌표들 : ',this.dongLatLng);
+      this.getResults(this.dongCode);
+      this.displayMarker(this.dongLatLng);
+      // console.log('!검색후 좌표들 : ',this.dongLatLng);
+    },
+
+
+
+
+    //현재 위,경도를 갱신하고, 현재 위치의 동코드, 주소를 갱신
+    updateLatLng() {
+      var latlng = this.map.getCenter(); 
+      this.lat = latlng.getLat();
+      this.lng = latlng.getLng();
+      
+      //좌표를 이용해서 법정동을 반환받는 요청
+      axios.get(
+          `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${this.lng}&y=${this.lat}`,
+          { headers: { Authorization: `KakaoAK c46f27eaebc3444220e50470b5d06e52` } }
+      ).then((result) => {
+        //동코드 갱신
+        // console.log(result);
+        // console.log(result.data);
+        // console.log(result.data.documents[0].code);
+        this.dongCode = result.data.documents[0].code;
+
+        //화면에 출력할 주소 갱신
+        this.address =result.data.documents[0].address_name;
+        console.log('1ㄴ현재 주소 : ',this.address);
+        console.log('1ㄴ동코드 : ',this.dongCode);
+
+      }).catch(({ response }) => {
+          alert(response.data);
+          console.log('오류 떴다잉..');
+          console.log(response);
+      });
+
+
+      console.log('현재 주소 : ',this.address);
+      console.log('동코드 : ',this.dongCode);
+    },
+
+
+
+
     initMap() {
       const container = document.getElementById("map");
       const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        center: new kakao.maps.LatLng(37.50123567372594, 127.03949437304006),
         level: 5,
       };
 
@@ -80,17 +193,25 @@ export default {
       //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
       this.map = new kakao.maps.Map(container, options);
     },
+
+
+
+
     changeSize(size) {
       const container = document.getElementById("map");
       container.style.width = `${size}px`;
       container.style.height = `${size}px`;
       this.map.relayout();
     },
+
+
+
+
     displayMarker(markerPositions) {
-      console.log('위경도 출력하면 어떻게 나올까? ',markerPositions);
+      console.log('displayMarker() 호출');
+      // console.log('위경도 출력하면 어떻게 나올까? ',markerPositions);
       //console.log('이거 출력 되는거임?? ',this.dongLatLng[0]);
-
-
+      // this.markers=[];
       if (this.markers.length > 0) {
         this.markers.forEach((marker) => marker.setMap(null));
       }
@@ -100,22 +221,123 @@ export default {
       );
 
       if (positions.length > 0) {
-        this.markers = positions.map(
-          (position) =>
-            new kakao.maps.Marker({
-              map: this.map,
-              position,
-            })
-        );
+        console.log('마커 출력해보기 : ',this.markers);
+        console.log(positions);
+        console.log(positions[0]);
 
-        const bounds = positions.reduce(
-          (bounds, latlng) => bounds.extend(latlng),
-          new kakao.maps.LatLngBounds()
-        );
+        var imageSrc = 'C:/SSAFY/ProjectFinal-buyhome/buyhome-frontend-map/src/assets/logo.png', // 마커이미지의 주소입니다    
+          imageSize = new kakao.maps.Size(40, 44), // 마커이미지의 크기입니다
+          imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+    
 
-        this.map.setBounds(bounds);
+        for(var i=0; i<positions.length; i++){
+          var marker = new kakao.maps.Marker({
+                              map: this.map,
+                              position : positions[i],
+                              image: markerImage // 마커이미지 설정 
+                            });
+          this.markers.push(marker);
+          
+          //윈도우 만들어주기
+          // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+          var iwContent = '<div style="padding:5px;">Hello World!</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+
+          // 인포윈도우를 생성합니다
+          var infowindow = new kakao.maps.InfoWindow({
+              content : iwContent
+          });
+
+          // 마커에 마우스오버 이벤트를 등록합니다
+          kakao.maps.event.addListener(this.markers[i], 'mouseover', function() {
+            // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+              infowindow.open(this.map, this.markers[i]);
+              console.log('마우스 올라갔다', this.markers[i].getPosition());
+          });
+
+          // 마커에 마우스아웃 이벤트를 등록합니다
+          kakao.maps.event.addListener(this.markers[i], 'mouseout', function() {
+              // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+              infowindow.close();
+              console.log('마우스 내렸다', this.markers[i].getPosition());
+          });
+
+          // console.log('선언할때이다', marker);
+          console.log('마커두개 같은거 나오냐? > 1',marker.getPosition());
+          console.log('마커두개 같은거 나오냐? > 2',this.markers[i].getPosition());
+          console.log(marker.getPosition() === this.markers[i].getPosition());
+        }
+        // this.markers = positions.map(
+        //   (position) =>
+        //     new kakao.maps.Marker({
+        //       map: this.map,
+        //       position,
+        //     })
+        // );
+        // positions.map(
+        //   (position) =>
+        //     console.log(position)
+        // );
+        
+        console.log('마커 출력해보기2 : ',this.markers);
+
+        // const bounds = positions.reduce(
+        //   (bounds, latlng) => bounds.extend(latlng),
+        //   new kakao.maps.LatLngBounds()
+        // );
+
+        // this.map.setBounds(bounds);
+
+
+        // // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+        // var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+        // mapOption = { 
+        //     center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        //     level: 3 // 지도의 확대 레벨
+        // };
+
+        // var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+        // //마커를 표시할 위치입니다 
+        // var position =  new kakao.maps.LatLng(33.450701, 126.570667);
+
+        // // 마커를 생성합니다
+        // var marker = new kakao.maps.Marker({
+        //   position: position
+        // });
+
+        // // 마커를 지도에 표시합니다.
+        // marker.setMap(this.map);
+
+        // // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+        // var iwContent = '<div style="padding:5px;">Hello World!</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+
+        // // 인포윈도우를 생성합니다
+        // var infowindow = new kakao.maps.InfoWindow({
+        //     content : iwContent
+        // });
+
+        // // 마커에 마우스오버 이벤트를 등록합니다
+        // kakao.maps.event.addListener(marker, 'mouseover', function() {
+        //   // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+        //     infowindow.open(this.map, marker);
+        //     console.log('마우스 올라갔다');
+        // });
+
+        // // 마커에 마우스아웃 이벤트를 등록합니다
+        // kakao.maps.event.addListener(marker, 'mouseout', function() {
+        //     // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+        //     infowindow.close();
+        //     console.log('마우스 내렸다');
+        // });
       }
     },
+
+
+
+
     displayInfoWindow() {
       if (this.infowindow && this.infowindow.getMap()) {
         //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
@@ -136,8 +358,26 @@ export default {
 
       this.map.setCenter(iwPosition);
     },
+
+
+
+    
   },
 };
+
+
+// // 마커를 표시할 위치입니다 
+// var position =  new kakao.maps.LatLng(33.450701, 126.570667);
+
+// // 마커를 생성합니다
+// var marker = new kakao.maps.Marker({
+//   position: position,
+//   clickable: true // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+// });
+// this.kakao.maps.event.addListener(marker, 'click', function() {
+//   console.log('hello~~~');
+// });
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/store/index.js
+++ src/store/index.js
@@ -28,28 +28,28 @@ export default new Vuex.Store({
   },
   mutations: {
     setHomeInfos(state, payload) {
-      console.log('[mutation].............homeInfos : ', payload);
+      // console.log('[mutation].............homeInfos : ', payload);
       state.homeInfos = payload;
     },
     setHomeInfo(state, payload) {
-      console.log('[mutation].............homeInfo : ', payload);
+      // console.log('[mutation].............homeInfo : ', payload);
       state.homeInfo = payload;
     },
     setResults(state, payload) {
-      console.log('[mutation].............results : ', payload);
+      // console.log('[mutation].............results : ', payload);
       state.results = payload;
     },
     setDongLatLng(state, payload) {
-      console.log('[mutation].............dongLatLng : ', payload);
+      // console.log('[mutation].............dongLatLng : ', payload);
       state.dongLatLng = payload;
     }
   },
   actions: {
     getHomeInfos({ commit }) { 
-      console.log('[actions] getHomeInfos 호출');
+      // console.log('[actions] getHomeInfos 호출');
       http.get("home/1111018300") //이게 제일 많이 나오는 동코드임ㅋㅋ
         .then(({ data }) => {
-          console.log('[actions]................................homeInfos:', data)
+          // console.log('[actions]................................homeInfos:', data)
           commit("setHomeInfos", data);
         })
         .catch(({ response }) => {
@@ -57,25 +57,31 @@ export default new Vuex.Store({
         });
     },
     getResults({ commit }, payload) { 
-      console.log('[actions]................................getResults 조회 조건:', payload);
+      
+      console.log('getResults() 호출');
+      // console.log('[actions]................................getResults 조회 조건:', payload);
       http.get(`home/${payload}`)
         .then(({ data }) => {
-          console.log('[actions]................................getResults:', data);
-          
-          var arr2 = new Array(); //2차원 배열 선언
-          var element = null; //2차원 배열 한칸마다 넣을 1차원 배열
-
-          for (var i = 0; i < data.length; i++){
-            // console.log(data[i].lat);
-            // console.log(data[i].lng);
-            element = new Array(); //배열선언
-            element.push(data[i].lat);
-            element.push(data[i].lng);
-            arr2.push(element); //2차원 배열에 넣기
+          // console.log('[actions]................................getResults:', data);
+          if (data.length > 0) {
+            var arr2 = new Array(); //2차원 배열 선언
+            var element = null; //2차원 배열 한칸마다 넣을 1차원 배열
+  
+            for (var i = 0; i < data.length; i++){
+              // console.log(data[i].lat);
+              // console.log(data[i].lng);
+              element = new Array(); //배열선언
+              element.push(data[i].lat);
+              element.push(data[i].lng);
+              arr2.push(element); //2차원 배열에 넣기
+            }
+            // console.log(arr2);
+  
+            commit("setDongLatLng", arr2);
+          } else {
+            alert('해당지역엔 매물이 없어유.. T^T;;')
+            commit("setDongLatLng", data);
           }
-          // console.log(arr2);
-
-          commit("setDongLatLng", arr2);
         })
         .catch(({ response }) => {
           alert(response.data);
